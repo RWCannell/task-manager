9a6ee754965ede677f68e6f346d9a44938fb4cc3
RWCannell <regancannell@gmail.com>
2025-03-15 22:46:21 +0200

    Added Thymeleaf dependency to pom.xml

--- taskmanager/src/main/java/com/learningwithregan/taskmanager/TaskController.java
+++ taskmanager/src/main/java/com/learningwithregan/taskmanager/TaskController.java
@@ -1,8 +1,14 @@
 package com.learningwithregan.taskmanager;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class TaskController {
-    
+
+    @GetMapping("/tasks")
+    public String getTasks() {
+        return "tasks";
+    }
 }
